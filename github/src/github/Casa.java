4af28fe84ede02b11566a5c22cb684be2ae36bb9
dw2 <dw2@DW1a-12>
2021-06-23 12:22:36 +0200

    Modificaciones 1

---
+++ github/src/github/Casa.java
@@ -0,0 +1,16 @@
+package github;
+
+public class Casa {
+    private float superficie;
+    private String calle;
+    private int codigoPostal;
+    
+    /**
+     * Un equipo de limpiadores deja la casa libre de gérmenes
+     * y desinfecta todas las superficies
+     * */    
+    public void limpiarLaCasa() {
+        String mensajito ="Te hemos limpiado la casa, está desinfectada";
+        System.out.println(mensajito);
+    }    
+}
